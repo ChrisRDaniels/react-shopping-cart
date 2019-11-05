04f6631e474133f1d07665cafbfd752c1a07726b
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2019-11-05 14:36:11 -0700

    removed unnecessary use params import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
-import { useParams } from 'react-router';
+
 
 //Contexts
 import { ProductContext } from './contexts/ProductContext';
